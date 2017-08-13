16deb6b23b991fc124242dc3f5320e8b7f00c403
NaYuRi <coflquf1@naver.com>
2017-08-13 17:48:24 +0900

    cal4

---
+++ src/Calculator4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+	public class Calculator4{
+
+	
+	public void aa(int a){
+	
+		
+	}
+	public static void main(String[] args) {
+		
+		Calsub4 tt = new Calsub4(); 
+		
+		tt.first(0);
+		System.out.println("초기 값은 0 이에요!\n");
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("수를 입력해주세요");
+		tt.a = scan.nextInt();
+		
+		while(true) {
+			
+			//Scanner scan =new Scanner(System.in);
+			System.out.println("연산자를 입력해주세요");
+			tt.d = scan.next();
+		
+  		tt.once();
+
+		}
+
+	}
+
+		}
+
+	
